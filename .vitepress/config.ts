528edef281168a1f48ec57fced41bbda66406bf7
Artem <nelkor@vk.com>
2026-05-10 19:39:53 +0300

    overview (#1)

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -7,6 +7,14 @@ export default defineConfig({
   description: 'Flashcards For Teachers',
   head: [['link', { rel: 'icon', type: 'image/png', href: '/icon.png' }]],
   themeConfig: {
+    sidebarMenuLabel: 'Меню',
+    returnToTopLabel: 'Наверх',
+    outlineTitle: 'На этой странице',
+    darkModeSwitchLabel: 'Тёмная тема',
+    docFooter: {
+      prev: 'Предыдущая страница',
+      next: 'Следующая страница',
+    },
     logo: {
       src: '/icon.png',
       alt: 'OnlyCards',
@@ -22,8 +30,22 @@ export default defineConfig({
         text: 'Продукт',
         items: [
           { text: 'Что такое OnlyCards', link: '/product/overview' },
+          { text: 'Глоссарий', link: '/product/glossary' },
+          { text: 'Для преподавателя', link: '/product/teacher-flow' },
+          { text: 'Для ученика', link: '/product/student-flow' },
         ],
-      }
+      },
+      {
+        text: 'Инструкции',
+        items: [
+          { text: 'Быстрый старт', link: '/guides/quick-start' },
+          { text: 'Варианты использования', link: '/guides/use-cases' },
+        ],
+      },
+      {
+        text: 'Часто задаваемые вопросы',
+        link: '/faq'
+      },
     ],
   },
 })
